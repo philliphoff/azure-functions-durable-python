7ea28577c918b62687dab679fc6c4d898f7bec75
Phillip Hoff <phoff@microsoft.com>
2025-08-08 15:53:47 -0700

    Try to simplify AI sample code.

--- samples-v2/durable_ai/function_app.py
+++ samples-v2/durable_ai/function_app.py
@@ -1,10 +1,24 @@
+import os
 import azure.functions as func
 import durable_ai
+from agents import Agent, Runner, OpenAIChatCompletionsModel, set_default_openai_client
+from openai import AsyncAzureOpenAI
 
 # Import the agent runners
 import deterministic
 import hello_world
 
+openai_client = AsyncAzureOpenAI(
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+        azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT")
+    )
+
+# Set the default OpenAI client for the Agents SDK
+set_default_openai_client(openai_client)
+
+model=OpenAIChatCompletionsModel(model="gpt-4.1", openai_client=openai_client)
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 ai_app = durable_ai.DurableAIFunctionApp(app)
@@ -13,7 +27,7 @@ ai_app.enable_agent_http_trigger()
 
 @ai_app.agent(name="hello_world")
 async def run_hello_world(input):
-    return await hello_world.run(input)
+    return await hello_world.run(input, model)
 
 @ai_app.agent(name="deterministic")
 async def run_deterministic(input):
--- samples-v2/durable_ai/hello_world/__init__.py
+++ samples-v2/durable_ai/hello_world/__init__.py
@@ -1,27 +1,16 @@
-import os
-import azure.functions as func
+from agents import Agent, Runner
+from agents.run import Model
 
-from agents import Agent, Runner, OpenAIChatCompletionsModel, set_default_openai_client
-from openai import AsyncAzureOpenAI
-
-async def run(input):
-    openai_client = AsyncAzureOpenAI(
-        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-        api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-        azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT")
-    )
-
-    # Set the default OpenAI client for the Agents SDK
-    set_default_openai_client(openai_client)
-
+async def run(input: str, model: Model) -> str:
     agent = Agent(
         name="Assistant",
         instructions="You are a helpful assistant",
-        model=OpenAIChatCompletionsModel(model="gpt-4.1", openai_client=openai_client))
+        model=model
+    )
 
     result = await Runner.run(
         agent,
-        input=input)
+        input=input
+    )
     
     return result.final_output
